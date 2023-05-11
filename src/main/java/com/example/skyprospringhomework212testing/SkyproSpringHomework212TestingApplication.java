bb160fd5e91cfcc0cdb4d5fd1114c7f539a22908
ZRoman87 <zrazhevskiy.roman@yandex.ru>
2023-05-11 21:09:23 +0300

    First commit

---
+++ src/main/java/com/example/skyprospringhomework212testing/SkyproSpringHomework212TestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.skyprospringhomework212testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringHomework212TestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyproSpringHomework212TestingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/skyprospringhomework212testing/SkyproSpringHomework212TestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skyprospringhomework212testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproSpringHomework212TestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
